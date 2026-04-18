d0a3814e6b2b324971dea5aae8676c1d32c5178f
Mac Jones Soriano <mac.soriano@dottystylecreative.com>
2026-04-18 12:36:41 +0800

    implement fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,22 +9,20 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
 	return (
 		<SafeAreaView className='flex-1 bg-background p-5'>
-			<Text className='text-xl font-bold text-success'>
-				Welcome to Nativewind!
-			</Text>
+			<Text className='text-5xl font-sans-extrabold'>Home</Text>
 			<Link
 				href='/onboarding'
-				className='mt-4 rounded bg-primary text-white p-4'>
+				className='mt-4 rounded bg-primary font-sans-bold text-white p-4'>
 				<Text>Go to Onboarding</Text>
 			</Link>
 			<Link
 				href='/(auth)/sign-in'
-				className='mt-4 rounded bg-primary text-white p-4'>
+				className='mt-4 rounded bg-primary font-sans-bold text-white p-4'>
 				<Text>Go to Sign In</Text>
 			</Link>
 			<Link
 				href='/(auth)/sign-up'
-				className='mt-4 rounded bg-primary text-white p-4'>
+				className='mt-4 rounded bg-primary font-sans-bold text-white p-4'>
 				<Text>Go to Sign Up</Text>
 			</Link>
 
